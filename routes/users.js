0f371e818e766ad8d35ff1249500b1145461265c
Felipe Figueroa <amenadiel@gmail.com>
2018-01-25 20:26:39 -0300

    displaying cards

--- routes/users.js
+++ routes/users.js
@@ -4,42 +4,23 @@ var express = require('express'),
 	_ = require('lodash'),
 	sqlite = require('sqlite'),
 	Promise = require('bluebird'),
-	dbPromise = sqlite.open('./cards.sqlite3', {
-		Promise,
-		cached: true
-	}),
 	rp = require('request-promise'),
 	debug = require('debug')('CR:users'),
 	env = process.env; // eslint-disable-line no-process-env
 
-
-dbPromise.then((db) => {
-	debug('DB is Open');
-	debug('API KEY IS ', env.API_KEY);
-	db.close();
+let dbPromise = sqlite.open('./cards.sqlite3', {
+	Promise,
+	cached: true
 });
 
-let insert_cards_stmt = `INSERT OR REPLACE INTO CARDS (
-	id,
-	name,
-	icon,
-	key,
-	rarity,
-	elixir,
-	type,
-	arena,
-	description) 
-VALUES (
-	?,
-	?,
-	?,
-	?,
-	?,
-	?,
-	?,
-	?,
-	?
-	);`;
+let insert_cards_stmt = `INSERT OR REPLACE INTO cards (
+	id,	name,	icon,	key,	rarity,	elixir,	type,	arena,	description) 
+	VALUES (?, ?,	?,	?,	?,	?,	?,	?,	?	);`,
+	insert_player_cards = `INSERT OR REPLACE INTO player_cards 
+	(id,player_tag,card_id, card_key,level, count, required_for_upgrade, left_to_upgrade)
+	VALUES (?,?,?,?,?,?,?,?);`,
+	insert_player = `INSERT OR REPLACE INTO player (tag,name) VALUES (?,?)`;
+
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -66,17 +47,36 @@ async function getResponse(playertag) {
 	return jsonres;
 }
 
-async function insertCards(cards) {
+async function insertCards(jsonres) {
+
+	dbPromise.then((db) => {
+		debug('DB is Open');
+		debug('API KEY IS ', env.API_KEY);
+	});
+
+
 	let db = await dbPromise,
+		player_insertion = await db.run(insert_player, jsonres.tag, jsonres.name),
 		stmt = await db.prepare(insert_cards_stmt);
 
 
-	_.each(cards, (card) => {
+	_.each(jsonres.cards, (card) => {
 		stmt.run(card.id, card.name, card.icon, card.key, card.rarity, card.elixir, card.type, card.arena, card.description);
-		debug(card);
+		debug(`upserting card ${card.key} into cards table`);
 	});
 
 	await stmt.finalize();
+
+	let stmt2 = await db.prepare(insert_player_cards);
+
+	_.each(jsonres.cards, (card) => {
+		stmt2.run(jsonres.tag + '|' + card.id, jsonres.tag, card.id, card.key, card.level, card.count, card.requiredForUpgrade, card.leftToUpgrade);
+		debug(`upserting card ${card.key} for player ${jsonres.tag} into player_cards table`);
+	});
+
+	await stmt2.finalize();
+
+
 	return;
 
 }
@@ -87,11 +87,77 @@ router.get('/:playertag', async (req, res, next) => {
 	var playertag = req.params.playertag;
 	try {
 		let jsonres = await getResponse(playertag);
-		await insertCards(jsonres.cards);
 
-		res.json(jsonres.cards);
+		await insertCards(jsonres);
+
+		res.json(_.pick(jsonres, [
+			'tag', 'name', 'trophies', 'games', 'chestCycle', 'cards'
+		]));
 	} catch (err) {
 		debug('cr-api ERROR');
+		_.each(err, (content, key) => {
+			debug(`${key} : ${content}`);
+		});
+		res.send(err.error);
+	}
+});
+
+/* beautify preserve:start */
+router.get('/cards/:playertag', async (req, res, next) => {
+/* beautify preserve:end */
+	var playertag = req.params.playertag;
+	try {
+
+		dbPromise.then((db) => {
+			debug('DB is Open');
+			debug('API KEY IS ', env.API_KEY);
+		});
+
+
+		let db = await dbPromise,
+			cards = await db.all(
+				`SELECT 
+				cards.name,
+				cards.icon,
+				cards.rarity,
+				cards.elixir,
+				cards.type,
+				player_cards.level, 
+				player_cards.required_for_upgrade 
+				FROM player
+				JOIN player_cards ON player.tag = player_cards.player_tag
+				JOIN cards ON cards.id = player_cards.card_id
+				WHERE player.tag=?`,
+				playertag);
+
+		_.each(cards, (card) => {
+			if (
+				(card.rarity === 'Epic' && card.level < 4) ||
+				(card.rarity === 'Rare' && card.level < 7) ||
+				(card.rarity === 'Common' && card.level < 9)
+			) {
+				card.comment = 'Below tournament standard';
+				card.tournament = 'below';
+			} else if (
+				(card.rarity === 'Legendary' && card.level > 1) ||
+				(card.rarity === 'Epic' && card.level > 4) ||
+				(card.rarity === 'Rare' && card.level > 7) ||
+				(card.rarity === 'Common' && card.level > 9)
+			) {
+				card.comment = 'Above tournament standard';
+				card.tournament = 'above';
+			} else {
+				card.comment = 'On tournamet standard';
+				card.tournament = 'pair';
+			}
+		});
+
+		res.render('cards', {
+			cards: cards
+		});
+
+	} catch (err) {
+		debug('ERROR requesting player cards');
 		_.each(err, (content, key) => {
 			debug(`${key} : ${content}`);
 		});
